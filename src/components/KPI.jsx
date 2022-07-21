da3e0267292572954dc1273be4b950166285b542
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-07-21 22:25:13 +0200

    liaison avec les données du service

--- src/components/KPI.jsx
+++ src/components/KPI.jsx
@@ -1,38 +1,19 @@
 import React from 'react';
 import { RadialBarChart, RadialBar, PolarAngleAxis } from 'recharts';
 
-function KPI() {
-    const data = [
-        {
-            uv: 12, fill: '#ff0000',
-        },
-        
-        // {
-        //     name: '30-34', uv: 15.69, pv: 1398, fill: '#8dd1e1',
-        // },
-        // {
-        //     name: '35-39', uv: 8.22, pv: 9800, fill: '#82ca9d',
-        // },
-        // {
-        //     name: '40-49', uv: 8.63, pv: 3908, fill: '#a4de6c',
-        // },
-        // {
-        //     name: '50+', uv: 2.63, pv: 4800, fill: '#d0ed57',
-        // },
-        // {
-        // 	name: 'unknow', uv: 6.67, pv: 4800, fill: '#ffc658',
-        // },
-    ];
-
+function KPI( {dataKPI} ) {
+    
+    const percentScore = dataKPI.todayScore * 100 ;
+    console.log(dataKPI)
     return (
         <div className='kpi-chart'>
             <h3 className='title-chart'>Score</h3>
-            <RadialBarChart width={260} height={260} cx={150} cy={150} innerRadius={100} radius={10} barSize={10} data={data}>
-				<RadialBar minAngle={15} label={{ position: 'insideStart', fill: '#fff' }} background clockWise dataKey="uv" />
-                <PolarAngleAxis type='number' domain={[0, 100]} tick={false} />
+            <RadialBarChart width={260} height={260} cx={150} cy={150} innerRadius={100} radius={10} barSize={10} data={[dataKPI]}>
+				<RadialBar minAngle={15} fill='#ff0000' background cornerRadius={25} dataKey='todayScore' />
+                <PolarAngleAxis type='number' domain={[0, 1]} tick={false} />
 			</RadialBarChart>
             <div className='kpi-chart-text'>
-                <p className='kpi-chart-text-percent'>12%</p>
+                <p className='kpi-chart-text-percent'>{percentScore}%</p>
                 <p className='kpi-chart-text-name'>de votre objectif</p>
             </div>
         </div>
