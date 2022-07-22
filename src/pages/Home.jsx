76671f51357233746095eaabea37b666aaae5f51
Theo Puteaux <theo.ptx@laposte.net>
2022-07-22 17:40:32 +0200

    ajout d'une verification des data ligne 37-39

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 //import getData from '../service/serviceMock.js'
-import getFetchData from '../service/serviceAPI.js'
+import getData from '../service/serviceAPI.js'
 import Hello from '../components/Hello.jsx'
 import KPI from '../components/KPI.jsx'
 import Menu from '../components/Menu.jsx'
@@ -9,6 +9,7 @@ import AverageSession from '../components/AverageSession.jsx'
 import PersonnalStat from '../components/PersonnalStat.jsx'
 import Activity from '../components/Activity.jsx'
 import Perfomance from '../components/Performance.jsx'
+
 function Home() {
 
     let {id} = useParams ();
@@ -19,21 +20,24 @@ function Home() {
 
 
     useEffect(() => {
-        getFetchData(id, 'main')
+        getData(id)
             .then(data => setUserMainData(data))
             .catch( err => console.log(" Erreur survenue lors de récupération des données ", err))
-        getFetchData(id, 'activity')
+        getData(id, 'activity')
             .then(data => setUserDataActivity(data))
             .catch( err => console.log(" Erreur survenue lors de la recuperation des données Activity", err))
-        getFetchData(id, 'average-sessions')
+        getData(id, 'average-sessions')
             .then(data => setUserDataAverageSession(data))
             .catch( err => console.log(" Erreur survenu lors de la récuperation des données AverageSession", err))
-        getFetchData(id, 'performance')
+        getData(id, 'performance')
             .then(data => setUserDataPerformance(data))
             .catch( err => console.log(" Erreur survenu lors de la recuperation des données Performance", err))
     }, [id]);
-    
-    console.log(getFetchData(id,'main'))
+
+    if(!userMainData || !userDataActivity || !userDataAverageSession || !userDataPerformance){
+        return null;
+    }
+
     return (
         <div>
             <Menu/>
