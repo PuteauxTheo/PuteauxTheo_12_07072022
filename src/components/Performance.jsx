d15be61d50a33f57f49975766d4ee8fc20c97e04
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-07-22 22:34:27 +0200

    changement de la police

--- src/components/Performance.jsx
+++ src/components/Performance.jsx
@@ -6,23 +6,21 @@ function Perfomance( { dataPerformance } ) {
 
   const categoriesPerformance = (kind) =>{
     switch (kind) {
-      case 1: return 'cardio';
-      case 2: return 'energy';
-      case 3: return 'endurance';
-      case 4: return 'strength';
-      case 5: return 'speed';
-      case 6: return 'intensity';
+      case 1: return 'Cardio';
+      case 2: return 'Energie';
+      case 3: return 'Endurance';
+      case 4: return 'Force';
+      case 5: return 'Vitesse';
+      case 6: return 'Intensité';
       default: return null;
     }
   } 
-
-  console.log(dataPerformance)
 
   return (
     <div className="radar-chart">
       <RadarChart cx={125} cy={100} outerRadius={70} width={260} height={260} data={dataPerformance} >
           <PolarGrid radialLines={false}/>
-          <PolarAngleAxis dataKey='kind' tickFormatter={categoriesPerformance}stroke="#fff" tickLine={false}/>
+          <PolarAngleAxis dataKey='kind' tickFormatter={categoriesPerformance} fontFamily={'Roboto'} fontSize={12} stroke="#fff" tickLine={false}/>
           <Radar dataKey="value" stroke="#ff0000" fill="#ff0000" fillOpacity={0.6}/>
       </RadarChart>
     </div>        
